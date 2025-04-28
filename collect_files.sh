f2d8802a7c3bfe1cbc6c8647a0a007ab3c832a5f
Margarita <123@>
2025-04-28 15:10:19 +0300

    Paper

--- collect_files.sh
+++ collect_files.sh
@@ -14,23 +14,31 @@ if [[ -z "$input" || -z "$output" ]]; then
 fi
 
 input="${input%/}"
-
 mkdir -p "$output"
 
-if [[ -n "$depth" ]]; then
-    files=$(find "$input" -maxdepth "$depth" -type f)
-else
-    files=$(find "$input" -type f)
-fi
-
-for file in $files; do
+find "$input" -type f | while read -r file; do
     rel_path="${file#$input/}"
     rel_path="${rel_path#/}"
     
-    dest_path="$output/$rel_path"
+    # Вычисляем глубину файла
+    slashes="${rel_path//[^\/]/}"
+    current_depth=$((${#slashes} + 1))
+    
+    if [[ -n "$depth" && $current_depth -gt $depth ]]; then
+        # Для файлов глубже максимальной глубины
+        IFS='/' read -ra parts <<< "$rel_path"
+
+        new_rel_path="depth3"
+        for ((i=$depth; i<${#parts[@]}; i++)); do
+            new_rel_path+="/${parts[i]}"
+        done
+    else
+        new_rel_path="$rel_path"
+    fi
+    
+    dest_path="$output/$new_rel_path"
     dest_dir=$(dirname "$dest_path")
     
     mkdir -p "$dest_dir"
-    
     cp "$file" "$dest_path"
 done
