f30454ab0b5c4e13586dea0f1ec947e84f045e49
Margarita <123@>
2025-04-30 15:55:40 +0300

    new_attempt

--- collect_files.sh
+++ collect_files.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
-if [[ "$1" == "--max_depth" ]]; then
-    depth="$2"
-    input="$3"
-    output="$4"
-else
-    depth=""
-    input="$1"
-    output="$2"
-fi
+depth=""
+input="$1"
+output="$2"
+useless="$3"
+maximal_depth="$4"
 
-if [[ -z "$input" || -z "$output" ]]; then
-    echo "Нужно указать директории!"
-    exit 1
-fi
+if [[ $useless == "" ]]
+    mkdir -p "$output"
 
-mkdir -p "$output"
-
-if [[ -n "$depth" ]]; then
-    files=$(find "$input" -maxdepth "$depth" -type f)
-else
-    files=$(find "$input" -type f)
-fi
-
-for file in $files; do
-    name=$(basename "$file")
-    path="$output/$name"
-
-    if [[ -e "$path" ]]; then
-        base="${name%.*}"
-        ext="${name##*.}"
-        n=1
-        while [[ -e "$output/${base}${n}.${ext}" ]]; do
-            n=$((n+1))
-        done
-        path="$output/${base}${n}.${ext}"
+    if [[ -n "$depth" ]]; then
+        files=$(find "$input" -maxdepth "$depth" -type f)
+    else
+        files=$(find "$input" -type f)
     fi
 
-    cp "$file" "$path"
-done
+
+    for file in $files; do
+        name=$(basename "$file")
+        path="$output/$name"
+
+        if [[ -e "$path" ]]; then
+            base="${name%.*}"
+            ext="${name##*.}"
+            n=1
+            while [[ -e "$output/${base}${n}.${ext}" ]]; do
+                n=$((n+1))
+            done
+            path="$output/${base}${n}.${ext}"
+        fi
+
+        cp "$file" "$path"
+    done
+fi
